377b6db29aaebf719222ef8280aa1e9a3b9d8c89
Rawand <rawandhawiz@gmail.com>
2015-12-28 13:15:02 +0000

    -added new tutorial files

---
+++ code/nolearn_tutorial.py
@@ -0,0 +1,118 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
+from lasagne.nonlinearities import softmax
+from lasagne.updates import adam
+from lasagne.layers import get_all_params
+
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import TrainSplit
+from nolearn.lasagne import objective
+
+
+def load_mnist(path):
+    X = []
+    y = []
+    with open(path, 'rb') as f:
+        next(f)  # skip header
+        for line in f:
+            yi, xi = line.split(',', 1)
+            y.append(yi)
+            X.append(xi.split(','))
+
+    # Theano works with fp32 precision
+    X = np.array(X).astype(np.float32)
+    y = np.array(y).astype(np.int32)
+
+    # apply some very simple normalization to the data
+    X -= X.mean()
+    X /= X.std()
+
+    # For convolutional layers, the default shape of data is bc01,
+    # i.e. batch size x color channels x image dimension 1 x image dimension 2.
+    # Therefore, we reshape the X data to -1, 1, 28, 28.
+    X = X.reshape(
+        -1,  # number of samples, -1 makes it so that this number is determined automatically
+        1,   # 1 color channel, since images are only black and white
+        28,  # first image dimension (vertical)
+        28,  # second image dimension (horizontal)
+    )
+
+    return X, y
+
+path = os.path.relpath('../data/mnist/train.csv')
+
+X, y = load_mnist(path)
+
+
+figs, axes = plt.subplots(4, 4, figsize=(6, 6))
+for i in range(4):
+    for j in range(4):
+        axes[i, j].imshow(-X[i + 4 * j].reshape(28, 28), cmap='gray', interpolation='none')
+        axes[i, j].set_xticks([])
+        axes[i, j].set_yticks([])
+        axes[i, j].set_title("Label: {}".format(y[i + 4 * j]))
+        axes[i, j].axis('off')
+
+layers0 = [
+    # layer dealing with the input data
+    (InputLayer, {'shape': (None, X.shape[1], X.shape[2], X.shape[3])}),
+
+    # first stage of our convolutional layers
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (MaxPool2DLayer, {'pool_size': 2}),
+
+    # second stage of our convolutional layers
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (MaxPool2DLayer, {'pool_size': 2}),
+
+    # two dense layers with dropout
+    (DenseLayer, {'num_units': 64}),
+    (DropoutLayer, {}),
+    (DenseLayer, {'num_units': 64}),
+
+    # the output layer
+    (DenseLayer, {'num_units': 10, 'nonlinearity': softmax}),
+]
+
+def regularization_objective(layers, lambda1=0., lambda2=0., *args, **kwargs):
+    # default loss
+    losses = objective(layers, *args, **kwargs)
+    # get the layers' weights, but only those that should be regularized
+    # (i.e. not the biases)
+    weights = get_all_params(layers[-1], regularizable=True)
+    # sum of absolute weights for L1
+    sum_abs_weights = sum([abs(w).sum() for w in weights])
+    # sum of squared weights for L2
+    sum_squared_weights = sum([(w ** 2).sum() for w in weights])
+    # add weights to regular loss
+    losses += lambda1 * sum_abs_weights + lambda2 * sum_squared_weights
+    return losses
+
+net0 = NeuralNet(
+    layers=layers0,
+    max_epochs=10,
+
+    update=adam,
+    update_learning_rate=0.0002,
+
+    objective=regularization_objective,
+    objective_lambda2=0.0025,
+
+    train_split=TrainSplit(eval_size=0.25),
+    verbose=1,
+)
+
+net0.fit(X, y)
